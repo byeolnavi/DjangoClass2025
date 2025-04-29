33105279d3c7a18b51cb167ec701e53b2e506f46
hanbyeol <rhgksquf@gmail.com>
2025-04-29 16:41:04 +0900

    urls.py 파일 수정

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 urlpatterns = [
     path('<int:pk>/', views.PostDetail.as_view()),
     path('',views.PostList.as_view()),
+    path('about_me/', include('single_pages.urls')),
+    path('landing/', include('single_pages.urls')),
     #path('',views.index),
     #path('<int:pk>/', views.single_post_page),
 ]
