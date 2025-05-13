2ddc61a96b3111ce1656b77716e102822e3d8585
hanbyeol <rhgksquf@gmail.com>
2025-05-13 17:12:41 +0900

    카테고리 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -17,10 +17,39 @@ class PostList(ListView):
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category = None).count()
         return context
+    
+    def category_page(request, slug) :
+        if slug == 'no_category' :
+            category = '미분류'
+            post_list = Post.objects.filter(category=None)
+        else :
+            category = Category.objects.get(slug=slug)
+            post_list = Post.objects.filter(category=category)
+
+        category = Category.objects.get(slug=slug)
+
+        return render(
+            request,
+            'blog/post_list.html',
+            {
+                'post_list': post_list,
+                'categories': Category.objects.all(),
+                'no_category_post_count': Post.objects.filter(category=None).count(),
+                'category' : category,
+            }
+        )
+    
+    
 
 class PostDetail(DetailView) :
     model = Post
     template_name = 'blog/post_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
 
 '''
 FBV로 페이지 만들기 
